7516a7387eee0f744bf63edec3517a1b7eab24a4
asstral842 <83505827+asstral842@users.noreply.github.com>
2021-05-01 19:54:37 +0200

    Add files via upload

---
+++ Untitled Script 7.js
@@ -0,0 +1,36 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: purple; icon-glyph: magic;
+// kick.js
+module.exports = {
+    run: async (message, args) => {
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        const member = message.mentions.members.first()
+        if (!member) return message.channel.send('Veuillez mentionner le membre à exclure.')
+        if (member.id === message.guild.ownerID) return message.channel.send('Vous ne pouvez pas exclure le propriétaire du serveur.')
+        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas exclure ce membre.')
+        if (!member.kickable) return message.channel.send('Le bot ne peut pas exclure ce membre.')
+        const reason = args.slice(1).join(' ') || 'Aucune raison fournie'
+        await member.kick(reason)
+        message.channel.send(`${member.user.tag} a été exclu !`)
+    },
+    name: 'kick',
+    guildOnly: true
+}
+
+// ban.js
+module.exports = {
+    run: async (message, args) => {
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        const member = message.mentions.members.first()
+        if (!member) return message.channel.send('Veuillez mentionner le membre à bannir.')
+        if (member.id === message.guild.ownerID) return message.channel.send('Vous ne pouvez pas bannir le propriétaire du serveur.')
+        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas Bannir ce membre.')
+        if (!member.bannable) return message.channel.send('Le bot ne peut pas bannir ce membre.')
+        const reason = args.slice(1).join(' ') || 'Aucune raison fournie'
+        await member.ban({reason})
+        message.channel.send(`${member.user.tag} a été banni !`)
+    },
+    name: 'ban',
+    guildOnly: true
+}

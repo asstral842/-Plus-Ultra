e69246a490ee1a8c31be6b6e6a25288028fac6c1
asstral842 <83505827+asstral842@users.noreply.github.com>
2021-05-02 10:20:10 +0200

    Update Token .js

--- Token .js
+++ Token .js
@@ -1,6 +1,3 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: light-brown; icon-glyph: magic;
 {
     "token": "t88cMeAGYl5TvkHn7NGma7UVuN1xLE-d"
 }

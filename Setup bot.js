a6de07c8745062e862ff8200ecc3c382d61c6451
asstral842 <83505827+asstral842@users.noreply.github.com>
2021-05-02 10:26:12 +0200

    Add files via upload

---
+++ Setup bot.js
@@ -0,0 +1,18 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: gray; icon-glyph: magic;
+
+{
+  "name": "⚡️• Plus Ultra",
+  "version": "1.0.0",
+  "description": "Bot Discord Server",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Asstral",
+  "license": "",
+  "dependencies": {
+    "discord.js": "^12.5.1"
+  }
+}

602e27a245799038c0c258bd81c9a0a2c274920e
asstral842 <83505827+asstral842@users.noreply.github.com>
2021-05-02 10:16:22 +0200

    Add files via upload

---
+++ Untitled Script 9.js
@@ -0,0 +1,18 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: purple; icon-glyph: magic;
+}
+ 
+// say.js
+const config = require('../config.json')
+ 
+module.exports = {
+    run: (message, args) => {
+        if (!message.member.hasPermission('BOTPERM')) return message.channel.send('Vous n\'avez pas la permission d\'utiliser cette commande.')
+        if (!args[0]) return message.channel.send('Veuillez indiquer du texte à envoyer.')
+        message.delete()
+        message.channel.send(message.content.trim().slice(`${config.prefix}say`.length))
+    },
+    name: 'say',
+    guildOnly: true
+}
